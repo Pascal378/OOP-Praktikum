54d117ffa3bf9b30a4f2bf6765f58c6f2e7b3d09
Pascal378 <pascal.schwartz@protonmail.com>
2022-10-22 19:52:28 +0200

    Translate comments to english

--- Praktikum_1/Ratespiel.java
+++ Praktikum_1/Ratespiel.java
@@ -3,28 +3,28 @@ package Praktikum_1;
 import java.util.Scanner;
 
 public class Ratespiel {
-    private static boolean found = false; //Wurde die Zahl erraten?
+    private static boolean found = false; //Boolean to check if the right number was guessed
 
     public static void main(String[] args) {
 
         int randomNumber = (int) (Math.random() * 101.0); //[0..100]
-        int counter = 0; //Counter zum Zählen der Versuche
+        int counter = 0; //Count the amount of tries
 
-        //Die Methoden werden so lange die Zahl nicht gefunden wurde wieder aufgerufen. Ein Counter zählt die Versuche
+        //As long as the correct number hasn't been found/guessed, ask for a new guess and check it directly - also counting the amount of tries
         while (!found) {
             counter++;
             pruefeZahl(rateZahl(), randomNumber, counter);
         }
     }
 
-    //Methode die neue Zahl abfragt und einliest
+    //Read in new number from user input
     private static int rateZahl() {
         System.out.println("Raten Sie eine Zahl: ");
         Scanner sc = new Scanner(System.in);
         return sc.nextInt();
     }
 
-    //Eingabe des Nutzers wird verglichen mit der Zufallszahl und entsprechende Anweisungen an den Nutzer ausgegeben
+    //Compare user input to the generated random number and print the corresponding output - in case of the right guess, "found" becomes true and the programm will stop
     private static void pruefeZahl(int guess, int random, int counter) {
         if (guess == random) {
             System.out.println("Geschafft! Zufallszahl war " + random);
@@ -37,3 +37,4 @@ public class Ratespiel {
         }
     }
 }
+
