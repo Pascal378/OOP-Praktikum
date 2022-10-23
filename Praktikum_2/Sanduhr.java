f0e5afd6215d893474745a85de267b177809e973
Pascal378 <pascal.schwartz@protonmail.com>
2022-10-23 17:45:09 +0200

    finish

--- Praktikum_2/Sanduhr.java
+++ Praktikum_2/Sanduhr.java
@@ -16,42 +16,48 @@ public class Sanduhr {
         int spaces = 0; //" "
         int asteriskCounter = width; // "*"
 
+        //Print lines according to the integer value in lineCounter
         for (int line = 0; line < lineCounter; line++) {
-
+            //Print spaces before asterisks
             for (int firstSpaces = 0; firstSpaces < spaces; firstSpaces++) {
                 System.out.print(" ");
             }
-
+            //Print asterisks
             for (int asterisk = 0; asterisk < asteriskCounter; asterisk++) {
                 System.out.print("*");
             }
             System.out.print("\n");
-            spaces++;
-            asteriskCounter -= 2;
+            spaces++; //Need 2 more spaces each line
+            asteriskCounter -= 2; //Need 2 fewer asterisks each line
         }
 
     }
 
     //Print lower half of the hourglass
     private static void lowerSanduhr(int width) {
+        //Initialize variables for an odd width
         int lineCounter = width / 2;
-
         int asteriskCounter = 3;
 
+        //Change variables in case of even width
         if (width % 2 == 0) {
             asteriskCounter = 4;
             lineCounter--;
         }
 
+        //Set amount of spaces needed later
         int spaces = (width - asteriskCounter) / 2;
 
         for (int line = 0; line < lineCounter; line++) {
+
             for (int firstSpaces = 0; firstSpaces < spaces; firstSpaces++) {
                 System.out.print(" ");
             }
+
             for (int asterisk = 0; asterisk < asteriskCounter; asterisk++) {
                 System.out.print("*");
             }
+
             System.out.print("\n");
             asteriskCounter += 2;
             spaces -= 1;
