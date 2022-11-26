f49be8c62af558d2ef45245f6c3de8adf749625f
Pascal378 <pascal.schwartz@protonmail.com>
2022-11-26 12:46:56 +0100

    add TemperatureGraph functionality

--- Praktikum_6/TemperatureGraph.java
+++ Praktikum_6/TemperatureGraph.java
@@ -1,4 +1,75 @@
 package Praktikum_6;
 
 public class TemperatureGraph {
+    int year;
+    int[] temperatures = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+
+    TemperatureGraph(int year) {
+        if (year >= 0) {
+            this.year = year;
+        } else {
+            System.out.println("Ungültige Jahreszahl");
+        }
+    }
+
+    void addTemperatur(int monat, int temperatur) {
+        if (monat < 0 && monat > 12) {
+            System.out.println("Ungültiger Monat.");
+            return;
+        }
+
+        if (temperatur < 0) {
+            System.out.println("Ungültige Temperatur.");
+            return;
+        }
+        this.temperatures[monat - 1] = temperatur;
+    }
+
+    void plotGraph() {
+        if (!checkTemperatures()) return;
+
+        System.out.println("Jahr: 2004");
+
+        for (int i = getMax(); i >= getMin(); i--) {
+
+            if (i < 10) System.out.print(i + "  ");
+            else System.out.print(i + " ");
+
+            for (int temperature : temperatures) {
+                if (temperature >= i) {
+                    System.out.print("# ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+
+    boolean checkTemperatures() {
+        for (int temperature : temperatures) {
+            if (temperature < 0) return false;
+        }
+
+        return true;
+    }
+
+
+    public int getMin() {
+        int min = temperatures[0];
+        for (int temperature : temperatures) {
+            if (temperature < min) min = temperature;
+        }
+
+        return min;
+    }
+
+    public int getMax() {
+        int max = temperatures[0];
+        for (int temperature : temperatures) {
+            if (temperature > max) max = temperature;
+        }
+
+        return max;
+    }
 }
--- Praktikum_6/TemperatureGraphTest.java
+++ Praktikum_6/TemperatureGraphTest.java
@@ -1,4 +1,23 @@
 package Praktikum_6;
 
 public class TemperatureGraphTest {
+    public static void main(String[] args) {
+        TemperatureGraph graph = new TemperatureGraph(2022);
+
+        graph.addTemperatur(1, 5);
+        graph.addTemperatur(2, 9);
+        graph.addTemperatur(3, 10);
+        graph.addTemperatur(4, 13);
+        graph.addTemperatur(5, 11);
+        graph.addTemperatur(6, 20);
+        graph.addTemperatur(7, 25);
+        graph.addTemperatur(8, 23);
+        graph.addTemperatur(9, 20);
+        graph.addTemperatur(10, 18);
+        graph.addTemperatur(11, 11);
+        graph.addTemperatur(12, 8);
+
+        graph.plotGraph();
+
+    }
 }
